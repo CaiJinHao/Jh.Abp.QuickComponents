9f38e92e7b528b039dbb2332119e2f1ce767a20b
Home <531003539@qq.com>
2020-10-25 18:43:05 +0800

    reference add

--- src/Jh.Abp.QuickComponents/obj/Release/netcoreapp3.1/Jh.Abp.QuickComponents.AssemblyInfo.cs
+++ src/Jh.Abp.QuickComponents/obj/Release/netcoreapp3.1/Jh.Abp.QuickComponents.AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("金浩")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Abp webapi项目需要使用的基础组件。Swagger、MIniProfiler、IdentityServer.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jh.Abp.QuickComponents")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jh.Abp.QuickComponents")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
