aa543e5204fac3f6d41fa6cc83c1b772adf7a2fa
caijinhao-company <531003539@qq.com>
2020-11-13 13:50:28 +0800

    处理apiversions swagger显示错误问题

--- src/Jh.Abp.QuickComponents/Jh/Abp/Swagger/SwaggerExtensions.cs
+++ src/Jh.Abp.QuickComponents/Jh/Abp/Swagger/SwaggerExtensions.cs
@@ -10,6 +10,8 @@ using Volo.Abp.Modularity;
 using Volo.Abp.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace Jh.Abp.QuickComponents.Swagger
 {
@@ -17,19 +19,19 @@ namespace Jh.Abp.QuickComponents.Swagger
     {
         public static IServiceCollection AddApiVersion(this IServiceCollection services)
         {
-             services.AddAbpApiVersioning(options =>
-             {
-                 options.ReportApiVersions = true;
+            services.AddAbpApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
                  //是否在没有填写版本号的情况下使用默认版本
                  options.AssumeDefaultVersionWhenUnspecified = true;
-                 options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.DefaultApiVersion = new ApiVersion(1, 0);
 
                  //options.ApiVersionReader = new HeaderApiVersionReader("api-version"); //Supports header too
                  //options.ApiVersionReader = new MediaTypeApiVersionReader(); //Supports accept header too
 
                  var mvcOptions = services.ExecutePreConfiguredActions<AbpAspNetCoreMvcOptions>();
-                 options.ConfigureAbp(mvcOptions);
-             });
+                options.ConfigureAbp(mvcOptions);
+            });
             return services;
         }
 
@@ -45,32 +47,45 @@ namespace Jh.Abp.QuickComponents.Swagger
                         Version = configuration["SwaggerApi:OpenApiInfo:Version"],
                         Description = configuration["SwaggerApi:OpenApiInfo:Description"],
                     });
-                    options.DocInclusionPredicate((docName, description) => {
-                        //使用下面的方式解决MapToApiVersion生成Doc文档错误问题
-                        //if (!description.TryGetMethodInfo(out MethodInfo methodInfo)) return false;
-                       description.TryGetMethodInfo(out MethodInfo methodInfo);
-                        // Get the MapToApiVersion attributes of the action
-                        var mapApiVersions = methodInfo
-                            .GetCustomAttributes(true)
-                            .OfType<MapToApiVersionAttribute>()
-                            .SelectMany(attr => attr.Versions);
-
-                        //if it contains MapToApiVersion attributes, then we should check those as the ApiVersion ones are ignored
-                        if (mapApiVersions.Any() && mapApiVersions.Any(v => $"v{v.ToString()}" == docName))
+                    options.DocInclusionPredicate((docName, description) =>
+                    {
+                        var _nowControllerAction = (ControllerActionDescriptor)description.ActionDescriptor;
+                        var _controller = _nowControllerAction.ControllerTypeInfo;
+                        var _nowHttpMethodAttr = _nowControllerAction.MethodInfo.GetCustomAttribute<HttpMethodAttribute>();
+                        var _nowMapToApiVersionAttr = _nowControllerAction.MethodInfo.GetCustomAttribute<MapToApiVersionAttribute>();
+                        if (_nowMapToApiVersionAttr==null)
+                        {
                             return true;
-
-                        // Get the ApiVersion attributes of the controller
-                        var versions = methodInfo.DeclaringType
-                            .GetCustomAttributes(true)
-                            .OfType<ApiVersionAttribute>()
-                            .SelectMany(attr => attr.Versions);
-                        return true;
-                        //return versions.Any(v => $"v{v.ToString()}" == docName);
+                        }
+                        //找到与当前方法时同一个HTTP的方法只显示最小版本的或者最大版本的
+                        //找到多个匹配的方法
+                        var equalsMethods = _controller.DeclaredMethods
+                        .Where(a => a.GetCustomAttributes<HttpMethodAttribute>().Contains(_nowHttpMethodAttr)).ToList();
+                        //从多个匹配的方法中找到最大的版本号的方法与当前方法判断是否匹配
+                        foreach (var item in equalsMethods)
+                        {
+                            var mapToApiVersion= item.GetCustomAttribute<MapToApiVersionAttribute>();
+                            //当前方法最大版本
+                            var versionMax = mapToApiVersion.Versions.OrderByDescending(a=>a.MajorVersion).ThenBy(a=>a.MinorVersion).First();
+                        }
+                        var httpMethods = equalsMethods
+                        .Select(a => a.GetCustomAttribute<MapToApiVersionAttribute>().Versions.OrderByDescending(a => a.MajorVersion).ThenBy(a => a.MinorVersion).First())
+                        .OrderByDescending(a=>a.MajorVersion).ThenBy(a=>a.MinorVersion).ToList();
+                        var maxVersion = httpMethods.FirstOrDefault();
+                        var _nowMaxVersion = _nowMapToApiVersionAttr.Versions.OrderByDescending(a => a.MajorVersion).ThenBy(a => a.MinorVersion).FirstOrDefault();
+                        if (_nowMaxVersion == maxVersion)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
                     });
                     options.CustomSchemaIds(type => type.FullName);
 
-                   //Swagger添加授权验证服务
-                   options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                    //Swagger添加授权验证服务
+                    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                     {
                         Description = "授权Token：Bearer Token",
                         Name = "Authorization",
@@ -93,6 +108,9 @@ namespace Jh.Abp.QuickComponents.Swagger
                             new string[] { }
                         }
                     });
+
+                    //options.SchemaFilter<SwaggerSchemaFilter>();
+                    //options.DocumentFilter<SwaggerDocumentFilter>();
                 });
         }
 
---
+++ src/Jh.Abp.QuickComponents/Jh/Abp/Swagger/SwaggerSchemaFilter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jh.Abp.QuickComponents.Swagger
+{
+    public class SwaggerSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+        }
+    }
+}
