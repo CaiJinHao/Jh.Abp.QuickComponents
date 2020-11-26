d7e5b91334a1b46a749c957b2f4e944d62807f36
caijinhao-company <531003539@qq.com>
2020-11-26 18:30:28 +0800

    ok

---
+++ src/Jh.Abp.Application.Contracts/Extensions/ICrudApplicationService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Jh.Abp.Extensions
+{
+    /// <summary>
+    /// 应用程序服务继承
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TEntityDto"></typeparam>
+    /// <typeparam name="TPagedRetrieveInputDto"></typeparam>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TRetrieveInputDto"></typeparam>
+    /// <typeparam name="TCreateInputDto"></typeparam>
+    /// <typeparam name="TUpdateInputDto"></typeparam>
+    /// <typeparam name="TDeleteInputDto"></typeparam>
+    public interface ICrudApplicationService<TEntity, TEntityDto, TPagedRetrieveInputDto, in TKey, in TRetrieveInputDto, in TCreateInputDto, in TUpdateInputDto, in TDeleteInputDto>
+    : ICrudAppService<TEntityDto, TPagedRetrieveInputDto, TKey, TRetrieveInputDto, TCreateInputDto, TUpdateInputDto>
+    {
+        /// <summary>
+        /// 通过对象条件删除,TODO:还没有写完
+        /// </summary>
+        /// <param name="deleteInput"></param>
+        /// <returns></returns>
+        Task DeleteAsync(TDeleteInputDto deleteInputDto);
+
+        //[Route("keys")]
+        //[HttpDelete]
+        /// <summary>
+        /// 删除多条主键数据
+        /// </summary>
+        /// <param name="keys"></param>
+        /// <returns></returns>
+        Task DeleteAsync(TKey[] keys);
+
+        /// <summary>
+        /// 更新部分有值更新没有值则不更新
+        /// </summary>
+        /// <param name="inputDto"></param>
+        /// <returns></returns>
+        Task<TEntity> UpdatePortionAsync(TKey key, TUpdateInputDto inputDto);
+
+        //[Route("1")]
+        //[HttpGet]
+        /// <summary>
+        /// 检索当前表数据 TODO:还没有写完
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        Task<List<TEntityDto>> GetModelsAsync(TRetrieveInputDto inputDto);
+    }
+}
---
+++ src/Jh.Abp.Application/Extensions/CrudApplicationService.cs
@@ -0,0 +1,138 @@
+﻿using Jh.Abp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Repositories;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+using System.Linq;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Application.Dtos;
+
+namespace Jh.Abp.Extensions
+{
+    public abstract class CrudApplicationService<TEntity, TEntityDto, TPagedRetrieveInputDto, TKey, TRetrieveInputDto, TCreateInputDto, TUpdateInputDto, TDeleteInputDto>
+        : CrudAppService<TEntity, TEntityDto, TPagedRetrieveInputDto, TKey, TRetrieveInputDto, TCreateInputDto, TUpdateInputDto>
+        , ICrudApplicationService<TEntity, TEntityDto, TPagedRetrieveInputDto, TKey, TRetrieveInputDto, TCreateInputDto, TUpdateInputDto, TDeleteInputDto>
+        where TEntity : class, IEntity<TKey>
+        where TEntityDto : IEntityDto<TKey>
+        where TPagedRetrieveInputDto : IEntityDto<TKey>
+    {
+        public CrudApplicationService(IRepository<TEntity, TKey> repository) : base(repository)
+        {
+        }
+
+        public async Task DeleteAsync(TDeleteInputDto deleteInputDto)
+        {
+            var delModels = await Repository.GetListAsync();
+            await DeleteAsync(delModels.Select(a => a.Id).ToArray());
+        }
+
+        public async Task DeleteAsync(TKey[] keys)
+        {
+            await Repository.DeleteAsync(a => keys.Contains(a.Id));
+        }
+
+        public async Task<List<TEntityDto>> GetModelsAsync(TRetrieveInputDto inputDto)
+        {
+            var query = ReadOnlyRepository;
+            var entities = await AsyncExecuter.ToListAsync(query);
+            return ObjectMapper.Map<List<TEntity>, List<TEntityDto>>(entities);
+        }
+
+        public async Task<TEntity> UpdatePortionAsync(TKey key, TUpdateInputDto updateInput)
+        {
+            var entity = await Repository.GetAsync(key);
+
+            var toFields = entity.GetType().GetProperties();
+            var formFields = updateInput.GetType().GetProperties();
+            foreach (var item in formFields)
+            {
+                var toField = toFields.Where(a => a.Name == item.Name).FirstOrDefault();
+                if (toField != null)
+                {
+                    var _value = item.GetValue(updateInput);
+                    if (_value != null)
+                    {
+                        var fieldType = _value.GetType();
+                        if (fieldType.IsEnum)
+                        {
+                            var _v = (int)_value;
+                            if (_v > 0)
+                            {
+                                toField.SetValue(entity, _value);
+                            }
+                        }
+                        else
+                        {
+                            switch (fieldType.Name)
+                            {
+                                case "String":
+                                    {
+                                        if (!string.IsNullOrEmpty((string)_value))
+                                        {
+                                            toField.SetValue(entity, _value);
+                                        }
+                                    }
+                                    break;
+                                case "DateTime":
+                                    {
+                                        var _v = (DateTime)_value;
+                                        if (_v > new DateTime(1900, 1, 1))
+                                        {
+                                            toField.SetValue(entity, _value);
+                                        }
+                                    }
+                                    break;
+                                case "Int16":
+                                    {
+                                        var _v = (Int16)_value;
+                                        if (_v > 0)
+                                        {
+                                            toField.SetValue(entity, _value);
+                                        }
+                                    }
+                                    break;
+                                case "Int32":
+                                    {
+                                        var _v = (Int32)_value;
+                                        if (_v > 0)
+                                        {
+                                            toField.SetValue(entity, _value);
+                                        }
+                                    }
+                                    break;
+                                case "Int64":
+                                    {
+                                        var _v = (Int64)_value;
+                                        if (_v > 0)
+                                        {
+                                            toField.SetValue(entity, _value);
+                                        }
+                                    }
+                                    break;
+                                case "Decimal":
+                                    {
+                                        var _v = (decimal)_value;
+                                        if (_v > 0)
+                                        {
+                                            toField.SetValue(entity, _value);
+                                        }
+                                    }
+                                    break;
+                                case "Boolean":
+                                    {
+                                        toField.SetValue(entity, _value);
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            return entity;
+        }
+    }
+}
