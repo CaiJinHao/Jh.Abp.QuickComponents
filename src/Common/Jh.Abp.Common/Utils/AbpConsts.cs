94406f3e299dc213aa975fc97f93cc610715d2dd
caijinhao-company <531003539@qq.com>
2021-02-21 11:13:25 +0800

    template

--- src/Common/Jh.Abp.Common/Utils/AbpConsts.cs
+++ src/Common/Jh.Abp.Common/Utils/AbpConsts.cs
@@ -6,6 +6,6 @@ namespace Jh.Abp.Common
 {
     public class AbpConsts
     {
-        public const string SerilogOutputTemplate = "{NewLine}{NewLine}Date：{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}LogLevel：{Level}{NewLine}Message：{Message}{NewLine}{Exception}{NewLine}";
+        public const string SerilogOutputTemplate = "{NewLine}{NewLine}Date：{Timestamp:yyyy-MM-dd HH:mm:ss.fff}{NewLine}LogLevel：{Level}{NewLine}Message：{Message}{NewLine}{Exception}";
     }
 }
