0ad17c5e2e68de03d25a32ab57ec65ddaae4546a
caijinhao-company <531003539@qq.com>
2021-02-24 16:09:24 +0800

    添加自动引用jh.abp

---
+++ src/Menu/install_package.bat
@@ -0,0 +1,7 @@
+dotnet add ./src/EquipmentQuotationApp.Application/EquipmentQuotationApp.Application.csproj package Jh.Abp.Application
+dotnet add ./src/EquipmentQuotationApp.Application.Contracts/EquipmentQuotationApp.Application.Contracts.csproj package Jh.Abp.Application.Contracts
+dotnet add ./src/EquipmentQuotationApp.Domain/EquipmentQuotationApp.Domain.csproj package Jh.SourceGenerator.Common
+dotnet add ./src/EquipmentQuotationApp.EntityFrameworkCore/EquipmentQuotationApp.EntityFrameworkCore.csproj package Jh.Abp.EntityFrameworkCore
+dotnet add ./host/EquipmentQuotationApp.HttpApi.Host/EquipmentQuotationApp.HttpApi.Host.csproj package Jh.Abp.MenuManagement.HttpApi
+dotnet add ./host/EquipmentQuotationApp.HttpApi.Host/EquipmentQuotationApp.HttpApi.Host.csproj package Jh.Abp.QuickComponents 
+dotnet add ./host/EquipmentQuotationApp.IdentityServer/EquipmentQuotationApp.IdentityServer.csproj package Jh.Abp.IdentityServer 
