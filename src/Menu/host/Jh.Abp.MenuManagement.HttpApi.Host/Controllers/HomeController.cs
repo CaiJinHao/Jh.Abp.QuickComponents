b89ad81f55fe10a2d6f4ad71e3a08ed3688027af
KimHo-Magicbook <531003539@qq.com>
2021-03-08 12:48:31 +0800

    ok

--- src/Menu/host/Jh.Abp.MenuManagement.HttpApi.Host/Controllers/HomeController.cs
+++ src/Menu/host/Jh.Abp.MenuManagement.HttpApi.Host/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Jh.Abp.MenuManagement.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : AbpController
     {
         public ActionResult Index()
