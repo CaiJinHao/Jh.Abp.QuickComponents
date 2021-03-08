f4a4eb5a9190495f1cb07f726b878b77a1efaf35
KimHo-Magicbook <531003539@qq.com>
2021-03-08 11:15:28 +0800

    m v

--- src/Menu/src/Jh.Abp.MenuManagement.Application/Menus/Menu/MenuAppService.cs
+++ src/Menu/src/Jh.Abp.MenuManagement.Application/Menus/Menu/MenuAppService.cs
@@ -24,14 +24,18 @@ namespace Jh.Abp.MenuManagement.Menus
 
         private readonly IMenuAndRoleMapRepository menuAndRoleMapRepository;
 
-        private IMenuAndRoleMapAppService _menuAndRoleMapAppService;
-        public IMenuAndRoleMapAppService menuAndRoleMapAppService => LazyGetRequiredService(ref _menuAndRoleMapAppService);
+        private readonly IMenuAndRoleMapAppService menuAndRoleMapAppService;
 
-        public MenuAppService(IMenuRepository repository, IMenuDapperRepository menuDapperRepository, IMenuAndRoleMapRepository _menuAndRoleMapRepository, IMenuAndRoleMapDomainService _menuAndRoleMapDomainService) : base(repository)
+        public MenuAppService(IMenuRepository repository,
+            IMenuDapperRepository menuDapperRepository, 
+            IMenuAndRoleMapRepository _menuAndRoleMapRepository,
+            IMenuAndRoleMapAppService _menuAndRoleMapAppService,
+            IMenuAndRoleMapDomainService _menuAndRoleMapDomainService) : base(repository)
         {
             menuRepository = repository;
-            menuAndRoleMapRepository = _menuAndRoleMapRepository;
             MenuDapperRepository = menuDapperRepository;
+            menuAndRoleMapRepository = _menuAndRoleMapRepository;
+            menuAndRoleMapAppService = _menuAndRoleMapAppService;
         }
 
         [UnitOfWork]
