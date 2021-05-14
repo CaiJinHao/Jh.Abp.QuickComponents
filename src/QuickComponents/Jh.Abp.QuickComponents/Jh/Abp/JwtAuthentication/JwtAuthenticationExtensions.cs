df3db1d625dc774c64d5a9217520c5b8cf1cda7b
KimHo-Magicbook <531003539@qq.com>
2021-05-14 15:34:12 +0800

    ok

--- src/QuickComponents/Jh.Abp.QuickComponents/Jh/Abp/JwtAuthentication/JwtAuthenticationExtensions.cs
+++ src/QuickComponents/Jh.Abp.QuickComponents/Jh/Abp/JwtAuthentication/JwtAuthenticationExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -37,6 +38,37 @@ namespace Jh.Abp.QuickComponents.JwtAuthentication
                 //options.Filters.Add(new AuthorizeFilter(policy));//添加权限过滤器
                 options.Filters.Add(new JhAuthorizationFilter(policy,configuration));
             });
+            return services;
+        }
+
+        //web 需要去除AbpAccountWebModule依赖
+        public static IServiceCollection AddAuthorizeOidc(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookies", options =>
+                {
+                    options.ExpireTimeSpan = TimeSpan.FromDays(365);
+                })
+                .AddAbpOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = configuration["AuthServer:Authority"];
+                    options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
+                    options.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+
+                    options.ClientId = configuration["AuthServer:ClientId"];
+                    options.ClientSecret = configuration["AuthServer:ClientSecret"];
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.Scope.Add("role");
+                    options.Scope.Add("email");
+                    options.Scope.Add("phone");
+                });
             return services;
         }
     }
