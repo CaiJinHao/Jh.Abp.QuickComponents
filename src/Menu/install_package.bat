97aa2e203c2d106eda8ea3f68092c5e588a840c5
Kimho <531003539@qq.com>
2021-05-17 12:52:44 +0800

    intall.bat

--- src/Menu/install_package.bat
+++ src/Menu/install_package.bat
@@ -1,8 +1,10 @@
-dotnet add ./src/WisdomGrainPlatform.Application/WisdomGrainPlatform.Application.csproj package Jh.Abp.Application
-dotnet add ./src/WisdomGrainPlatform.Application.Contracts/WisdomGrainPlatform.Application.Contracts.csproj package Jh.Abp.Application.Contracts
-dotnet add ./src/WisdomGrainPlatform.Domain/WisdomGrainPlatform.Domain.csproj package Jh.SourceGenerator.Common
-dotnet add ./src/WisdomGrainPlatform.EntityFrameworkCore/WisdomGrainPlatform.EntityFrameworkCore.csproj package Jh.Abp.EntityFrameworkCore
-dotnet add ./host/WisdomGrainPlatform.HttpApi.Host/WisdomGrainPlatform.HttpApi.Host.csproj package Jh.Abp.MenuManagement.HttpApi
-dotnet add ./host/WisdomGrainPlatform.HttpApi.Host/WisdomGrainPlatform.HttpApi.Host.csproj package Jh.Abp.QuickComponents 
-dotnet add ./host/WisdomGrainPlatform.Web.Unified/WisdomGrainPlatform.Web.Unified.csproj package Jh.Abp.QuickComponents 
-dotnet add ./host/WisdomGrainPlatform.IdentityServer/WisdomGrainPlatform.IdentityServer.csproj package Jh.Abp.IdentityServer 
+dotnet add ./src/Jh.Abp.MenuManagement.Application/Jh.Abp.MenuManagement.Application.csproj package Jh.Abp.Application
+dotnet add ./src/Jh.Abp.MenuManagement.Application.Contracts/Jh.Abp.MenuManagement.Application.Contracts.csproj package Jh.Abp.Application.Contracts
+dotnet add ./src/Jh.Abp.MenuManagement.Domain/Jh.Abp.MenuManagement.Domain.csproj package Jh.SourceGenerator.Common
+dotnet add ./src/Jh.Abp.MenuManagement.EntityFrameworkCore/Jh.Abp.MenuManagement.EntityFrameworkCore.csproj package Jh.Abp.EntityFrameworkCore
+dotnet add ./host/Jh.Abp.MenuManagement.HttpApi.Host/Jh.Abp.MenuManagement.HttpApi.Host.csproj package Jh.Abp.QuickComponents 
+dotnet add ./host/Jh.Abp.MenuManagement.IdentityServer/Jh.Abp.MenuManagement.IdentityServer.csproj package Jh.Abp.IdentityServer 
+dotnet add ./src/Jh.Abp.MenuManagement.HttpApi/Jh.Abp.MenuManagement.HttpApi.csproj package Jh.Abp.MenuManagement.HttpApi
+
+
+# Use MySql
