309b6f16a5280e1008beb2505412ee7ac5ffc4f9
Kimho <531003539@qq.com>
2021-05-19 14:47:30 +0800

    sso debug

---
+++ src/Menu/host/SSOTestHost/Startup.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSOTestHost
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            AddOidcAuthentication(services, Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+
+
+        public  IServiceCollection AddOidcAuthentication( IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookies", options =>
+                {
+                    var Hours = configuration["AuthServer:CookieExpireTimeSpanHours"];
+                    options.ExpireTimeSpan = TimeSpan.FromHours(Convert.ToInt32(Hours));
+                })
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = configuration["AuthServer:Authority"];
+                    options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
+                    options.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+
+                    options.ClientId = configuration["AuthServer:ClientId"];
+                    options.ClientSecret = configuration["AuthServer:ClientSecret"];
+
+                    options.SaveTokens = true;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    var scopeStr = configuration["AuthServer:Scope"];
+                    if (!string.IsNullOrWhiteSpace(scopeStr))
+                    {
+                        var scopes = scopeStr.Split(" ");
+                        foreach (var item in scopes)
+                        {
+                            if (!string.IsNullOrWhiteSpace(item))
+                            {
+                                options.Scope.Add(item);
+                            }
+                        }
+                    }
+                });
+            return services;
+        }
+    }
+}
