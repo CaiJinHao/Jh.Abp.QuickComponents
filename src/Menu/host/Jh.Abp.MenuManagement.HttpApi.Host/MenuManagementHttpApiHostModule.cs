8b5548e2abfbd64de498735e0652b377d928cd4c
caijinhao <531003539@qq.com>
2021-06-15 18:11:28 +0800

    ok

--- src/Menu/host/Jh.Abp.MenuManagement.HttpApi.Host/MenuManagementHttpApiHostModule.cs
+++ src/Menu/host/Jh.Abp.MenuManagement.HttpApi.Host/MenuManagementHttpApiHostModule.cs
@@ -182,11 +182,14 @@ namespace Jh.Abp.MenuManagement
             //审计日志配置
             Configure<AbpAuditingOptions>(options =>
             {
-                options.ApplicationName = "MunuManagement";
-                options.IsEnabledForGetRequests = true;
-                options.IsEnabledForAnonymousUsers = false;
-                options.AlwaysLogOnException = false;
-                //options.EntityHistorySelectors.AddAllEntities();
+                options.ApplicationName = "MunuManagement";//如果有多个应用程序保存审计日志到单一的数据库,使用此属性设置为你的应用程序名称区分不同的应用程序日志.
+                options.IsEnabled = true;// (默认值: true): 启用或禁用审计系统的总开关. 如果值为 false,则不使用其他选项
+                options.HideErrors = true;// (默认值: true): 在保存审计日志对象时如果发生任何错误,审计日志系统会将错误隐藏并写入常规日志. 如果保存审计日志对系统非常重要那么将其设置为 false 以便在隐藏错误时抛出异常.
+                options.IsEnabledForAnonymousUsers = true;//(默认值: true): 如果只想为经过身份验证的用户记录审计日志,请设置为 false.如果为匿名用户保存审计日志,你将看到这些用户的 UserId 值为 null.
+                options.AlwaysLogOnException = false;//(默认值: true): 如果设置为 true,将始终在异常/错误情况下保存审计日志,不检查其他选项(IsEnabled 除外,它完全禁用了审计日志).
+                options.IsEnabledForGetRequests = false;// (默认值: false): HTTP GET请求通常不应该在数据库进行任何更改,审计日志系统不会为GET请求保存审计日志对象. 将此值设置为 true 可为GET请求启用审计日志系统.
+                options.EntityHistorySelectors.AddAllEntities();    //选择器列表,用于确定是否选择了用于保存实体更改的实体类型
+                //options.IgnoredTypes = []; //审计日志系统忽略的 Type 列表. 如果它是实体类型,则不会保存此类型实体的更改. 在序列化操作参数时也使用此列表.
             });
 
             Configure<AbpAntiForgeryOptions>(options =>
