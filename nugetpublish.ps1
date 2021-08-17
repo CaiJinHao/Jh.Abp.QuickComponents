2d7b1573f1785b1a4de830dfa0aaa9a88506e07e
caijinhao <531003539@qq.com>
2021-08-17 13:41:25 +0800

    update version for abp version ok

--- nugetpublish.ps1
+++ nugetpublish.ps1
@@ -1,13 +1,13 @@
 $publishApiKey="123456"
 $publishSource="https://api.nuget.org/v3/index.json"
 
-$AbpExtensions="4.4.0001"	#4.4.0001
 $QuickComponents="4.4.1001"	#4.4.1001
 $SourceGenerator="4.4.2001"	#4.4.2001
+$AbpExtensions="4.4.3001"	#4.4.3001
 
 # 暂时用不到不再更新
-# $EntityFrameworkCoredm="0.2.0"	#4.4.3001
-# $MenuManagement="0.1.106"	#4.4.4001
+# $EntityFrameworkCoredm="0.2.0"	#4.4.4001
+# $MenuManagement="0.1.106"	#4.4.5001
 
 # AbpExtensions
 dotnet nuget push src\Jh.AbpExtensions\Jh.Abp.Application\bin\Release\Jh.Abp.Application.$AbpExtensions.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
@@ -30,21 +30,21 @@ dotnet nuget push src\GeneratorCoding\Jh.GeneratorCoding\bin\Release\Jh.SourceGe
 dotnet nuget push src\GeneratorCoding\Jh.SourceGenerator.Common\bin\Release\Jh.SourceGenerator.Common.$SourceGenerator.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
 
 # dm
-dotnet nuget push src\Jh.AbpExtensions\Jh.Abp.EntityFrameworkCore.Dm\bin\Release\Jh.Abp.EntityFrameworkCore.Dm.$EntityFrameworkCoredm.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Jh.AbpExtensions\Jh.Abp.EntityFrameworkCore.DmExtensionsCore\bin\Release\Jh.Abp.EntityFrameworkCore.DmExtensionsCore.$EntityFrameworkCoredm.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Jh.AbpExtensions\Jh.Abp.EntityFrameworkCore.Dm\bin\Release\Jh.Abp.EntityFrameworkCore.Dm.$EntityFrameworkCoredm.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Jh.AbpExtensions\Jh.Abp.EntityFrameworkCore.DmExtensionsCore\bin\Release\Jh.Abp.EntityFrameworkCore.DmExtensionsCore.$EntityFrameworkCoredm.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
 
 
 #menu
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Application\bin\Release\Jh.Abp.MenuManagement.Application.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Application.Contracts\bin\Release\Jh.Abp.MenuManagement.Application.Contracts.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Blazor\bin\Release\Jh.Abp.MenuManagement.Blazor.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Domain\bin\Release\Jh.Abp.MenuManagement.Domain.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Domain.Shared\bin\Release\Jh.Abp.MenuManagement.Domain.Shared.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.EntityFrameworkCore\bin\Release\Jh.Abp.MenuManagement.EntityFrameworkCore.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.HttpApi\bin\Release\Jh.Abp.MenuManagement.HttpApi.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.HttpApi.Client\bin\Release\Jh.Abp.MenuManagement.HttpApi.Client.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.MongoDB\bin\Release\Jh.Abp.MenuManagement.MongoDB.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
-dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Web\bin\Release\Jh.Abp.MenuManagement.Web.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Application\bin\Release\Jh.Abp.MenuManagement.Application.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Application.Contracts\bin\Release\Jh.Abp.MenuManagement.Application.Contracts.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Blazor\bin\Release\Jh.Abp.MenuManagement.Blazor.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Domain\bin\Release\Jh.Abp.MenuManagement.Domain.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Domain.Shared\bin\Release\Jh.Abp.MenuManagement.Domain.Shared.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.EntityFrameworkCore\bin\Release\Jh.Abp.MenuManagement.EntityFrameworkCore.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.HttpApi\bin\Release\Jh.Abp.MenuManagement.HttpApi.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.HttpApi.Client\bin\Release\Jh.Abp.MenuManagement.HttpApi.Client.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.MongoDB\bin\Release\Jh.Abp.MenuManagement.MongoDB.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
+#dotnet nuget push src\Menu\src\Jh.Abp.MenuManagement.Web\bin\Release\Jh.Abp.MenuManagement.Web.$MenuManagement.nupkg --api-key $publishApiKey --source $publishSource --skip-duplicate
 
 
 echo "success ok"
