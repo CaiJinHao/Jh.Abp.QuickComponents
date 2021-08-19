edc4c99bd529ac9131aa5e05ac7be9d69cfbe8b5
caijinhao <531003539@qq.com>
2021-08-19 22:50:20 +0800

    up version

--- nugetpublish.ps1
+++ nugetpublish.ps1
@@ -1,8 +1,8 @@
 $publishApiKey="123456"
 $publishSource="https://api.nuget.org/v3/index.json"
 
-$QuickComponents="4.4.1003"	#4.4.1001
-$SourceGenerator="4.4.2003"	#4.4.2001
+$QuickComponents="4.4.1004"	#4.4.1001
+$SourceGenerator="4.4.2004"	#4.4.2001
 $AbpExtensions="4.4.3003"	#4.4.3001
 
 # 暂时用不到不再更新
