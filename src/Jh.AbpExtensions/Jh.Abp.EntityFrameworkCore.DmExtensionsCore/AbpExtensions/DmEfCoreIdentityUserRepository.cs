955f2697e1da3130b7422bca13c45192fde4fff0
caijinhao <531003539@qq.com>
2021-11-22 17:30:27 +0800

    修复dm最新驱动问题

--- src/Jh.AbpExtensions/Jh.Abp.EntityFrameworkCore.DmExtensionsCore/AbpExtensions/DmEfCoreIdentityUserRepository.cs
+++ src/Jh.AbpExtensions/Jh.Abp.EntityFrameworkCore.DmExtensionsCore/AbpExtensions/DmEfCoreIdentityUserRepository.cs
@@ -22,33 +22,35 @@ namespace Jh.Abp.EntityFrameworkCore.DmExtensions.AbpExtensions
         {
         }
 
-        public override async Task<List<string>> GetRoleNamesAsync(Guid id, CancellationToken cancellationToken = default)
-        {
-            var dbContext = await GetDbContextAsync();
-            var query = from userRole in dbContext.Set<IdentityUserRole>()
-                        join role in dbContext.Roles on userRole.RoleId equals role.Id
-                        where userRole.UserId == id
-                        select role.Name;
-            var organizationUnitIds = dbContext.Set<IdentityUserOrganizationUnit>().Where(q => q.UserId == id).Select(q => q.OrganizationUnitId).ToArray();
+        //public override async Task<List<string>> GetRoleNamesAsync(Guid id, CancellationToken cancellationToken = default)
+        //{
+        //    var dbContext = await GetDbContextAsync();
+        //    var query = from userRole in dbContext.Set<IdentityUserRole>()
+        //                join role in dbContext.Roles on userRole.RoleId equals role.Id
+        //                where userRole.UserId == id
+        //                select role.Name;
+        //    var data= await query.ToListAsync(GetCancellationToken(cancellationToken));
+        //    return data;
+        //    //var organizationUnitIds = dbContext.Set<IdentityUserOrganizationUnit>().Where(q => q.UserId == id).Select(q => q.OrganizationUnitId).ToArray();
 
-            if (organizationUnitIds.Any())
-            {
-                var querytemp = from ouRole in dbContext.Set<OrganizationUnitRole>()
-                                join ou in dbContext.Set<OrganizationUnit>() on ouRole.OrganizationUnitId equals ou.Id
-                                where organizationUnitIds.Contains(ouRole.OrganizationUnitId)
-                                select ouRole.RoleId;
-                var organizationRoleIds = await(
-                    querytemp
-                ).ToListAsync(GetCancellationToken(cancellationToken));
+        //    //if (organizationUnitIds.Any())
+        //    //{
+        //    //    var querytemp = from ouRole in dbContext.Set<OrganizationUnitRole>()
+        //    //                    join ou in dbContext.Set<OrganizationUnit>() on ouRole.OrganizationUnitId equals ou.Id
+        //    //                    where organizationUnitIds.Contains(ouRole.OrganizationUnitId)
+        //    //                    select ouRole.RoleId;
+        //    //    var organizationRoleIds = await(
+        //    //        querytemp
+        //    //    ).ToListAsync(GetCancellationToken(cancellationToken));
 
-                var orgUnitRoleNameQuery = dbContext.Roles.Where(r => organizationRoleIds.Contains(r.Id)).Select(n => n.Name);
-                var resultQuery = query.Union(orgUnitRoleNameQuery);
-                return await resultQuery.ToListAsync(GetCancellationToken(cancellationToken));
-            }
-            else
-            {
-                return new List<string>();
-            }
-        }
+        //    //    var orgUnitRoleNameQuery = dbContext.Roles.Where(r => organizationRoleIds.Contains(r.Id)).Select(n => n.Name);
+        //    //    var resultQuery = query.Union(orgUnitRoleNameQuery);
+        //    //    return await resultQuery.ToListAsync(GetCancellationToken(cancellationToken));
+        //    //}
+        //    //else
+        //    //{
+        //    //    return new List<string>();
+        //    //}
+        //}
     }
 }
